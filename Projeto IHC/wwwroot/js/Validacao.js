3768d8f3bcc93e901096cb6343c9f3b2da509fee
GuiSilva77 <guilherme92325@gmail.com>
2022-05-31 16:18:40 -0300

    Alterações no site

--- Projeto IHC/wwwroot/js/Validacao.js
+++ Projeto IHC/wwwroot/js/Validacao.js
@@ -1,15 +1,34 @@
 ﻿const limparBTN = document.getElementById("limparBtn");
 
-document.getElementById("EnviarBtn").addEventListener("click", async () => {
-  //get all inputs
-  const inputs = document.querySelectorAll("input");
+const emBreve = document.getElementById("emBreve");
+const emCartaz = document.getElementById("emCartaz");
+const clas = document.getElementsById("clas");
+const res = document.getElementById("res");
 
-  inputs.forEach((input) => {
-    if (input.value == "") {
-      document.querySelector(`${input.id}-sp`).innerText = "Campo obrigatório";
-      return;
-    } else {
-      document.querySelector(`${input.id}-sp`).innerText = "";
-    }
-  });
+document.getElementById("EnviarBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (
+    (emBreve.checked == true && emCartaz.checked == true) ||
+    (emBreve.checked == false && emCartaz.checked == false)
+  ) {
+    document.getElementById("op-sp").innerText = "Selecione uma opção";
+    return;
+  }
+
+  if (clas.value == "0") {
+    document.getElementById("clas-sp").innerText =
+      "Selecione uma classificação";
+    return;
+  }
+
+  if (res.value == "" || res.value == null) {
+    document.getElementById("res-sp").innerText = "Insira o resumo";
+    return;
+  }
+
+  if (res != "" && res != null && clas.value != "0") {
+    const form = document.getElementById("form");
+    form.submit();
+  }
 });
